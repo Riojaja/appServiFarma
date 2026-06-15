488b1e61ab1f3c70b06e64379d3b1a5e1cfb0f4e
Rio2005 <rildoccanahuac@gmail.com>
2026-06-14 22:42:20 -0500

    inicio proyecto

---
+++ appServifarmaB/src/main/java/com/example/proyecto/app/AppServifarmaBApplication.java
@@ -0,0 +1,14 @@
+package com.example.proyecto.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServifarmaBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppServifarmaBApplication.class, args);
+		System.out.println("hola mundo introduccion");
+	}
+
+}
