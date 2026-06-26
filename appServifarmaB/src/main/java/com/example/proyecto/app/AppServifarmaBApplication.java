010d0ebe1e69075412fd3f6aa111ab644e896ed4
232192-max <232192@unamba.edu.pe>
2026-06-26 18:40:43 -0500

    backend

--- appServifarmaB/src/main/java/com/example/proyecto/app/AppServifarmaBApplication.java
+++ appServifarmaB/src/main/java/com/example/proyecto/app/AppServifarmaBApplication.java
@@ -8,7 +8,7 @@ public class AppServifarmaBApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppServifarmaBApplication.class, args);
-		System.out.println("hola mundo introduccion");
+		System.out.println("hola mundo cruel");
 	}
 
 }
